0d6c741d1e9ad20ca21956f31ee43a019939a9d1
Joshan Rai <pradheon@gmail.com>
2022-10-17 14:30:21 -0700

    New: Added form, button, and placeholder image to index.html Added Web Speech API code to script.js Added new styling for html to style.css
    TODO:
    Fix bugs with frontend
    Continue work on backend
    Connect tech stacks for backend

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Add JS here
+/***************************************************************************************************
+*                                      Web Speech API
+****************************************************************************************************/
+// Google Chrome Support for Web Speech API
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+//const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+// Define Speech Recognition instances
+const recognition = new SpeechRecognition();
+//const speechRecognitionList = new SpeechGrammarList();
+
+// Set properties of Speech Recognition instance
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+// Constants for page items
